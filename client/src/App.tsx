17288f7ab65398d1f0a722810b0bfaaa36eaef51
PeerPier <155882979+PeerPier@users.noreply.github.com>
2024-03-09 14:32:13 +0700

    font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import TestPage from "./Screens/test";
 import HomePage from "./Screens/home";
 import Navbar from "./Navbar/Navbar1";
 import Profile from "./Screens/profile";
+import WritePost from "./Screens/post";
 import EditProfile from "./Screens/edit-profille";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/nav" element={<Navbar />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/writepost" element={<WritePost />} />
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="/profile/edit-profile/:id" element={<EditProfile />} />
       </Routes>
